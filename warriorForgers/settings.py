6ef2147e58a4b2b138749192f4b0082dba3cb349
Divyanshu Khandelwal <67157526+Abhi-dr@users.noreply.github.com>
2023-09-10 08:43:03 +0530

    Update settings.py

--- warriorForgers/settings.py
+++ warriorForgers/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7s_&j+l#1o34!75bmhqz@%uv65h!)n$5pob9v0#9500xx23==7'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
